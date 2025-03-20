3f1afc7b742a0c3d78c6011c2141e2da0e511d21
SandeepSivakumar063 <sandeepps.22cse@kongu.edu>
2025-03-20 15:18:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u sandeepps1912 -p dckr_pat_3Vhxz0Ho3QIB48oWfV3v54RBdGE
+    docker login -u sandeepps1912 -p d-ckr_pat_3Vhxz0Ho3QIB48oWfV3v54RBdGE
     sudo docker tag test sandeepps1912/dev
     sudo docker push sandeepps1912/dev
+    docker-compose up -d
     
